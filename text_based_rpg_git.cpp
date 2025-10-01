2464cf5f0b623d9490296c569abb59aeaead25f7
Sans <16hrsstudyyy@gmail.com>
2025-10-01 21:48:31 +0530

    Update text_based_rpg_git.cpp

--- text_based_rpg_git.cpp
+++ text_based_rpg_git.cpp
@@ -1,2 +1,23 @@
-#include<stdio.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Forward declaration to solve circular dependency if Item has a Character pointer
+class Item; 
+
+// Base class for all living entities
+class Character {
+public:
+    // Constructor
+    Character(std::string name, int health, int strength)
+        : name(name), health(health), strength(strength) {}
+
+    // Virtual destructor for proper cleanup of derived classes
+    virtual ~Character() {} 
+
+    void attack(Character& target) {
+        std::cout << this->name << " attacks " << target.getName() << "!" << std::endl;
+        target.takeDamage(this->strength);
+    }
 
